78ea298e17f5f5659eaa0f56789089843062c6d6
sonnguyen16 <sonnv2212@gmail.com>
2024-10-04 21:59:52 +0700

    update

--- backend/database/migrations/2024_09_05_034224_create_personal_access_tokens_table.php
+++ backend/database/migrations/2024_09_05_034224_create_personal_access_tokens_table.php
@@ -13,7 +13,8 @@ return new class extends Migration
     {
         Schema::create('personal_access_tokens', function (Blueprint $table) {
             $table->id();
-            $table->morphs('tokenable');
+            $table->string('tokenable_type', 191);
+            $table->bigInteger('tokenable_id');
             $table->string('name');
             $table->string('token', 64);
             $table->text('abilities')->nullable();
