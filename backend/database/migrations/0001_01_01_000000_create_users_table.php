bb63c52a017cfb63e6fe9e7842e42d9260ccbb67
sonnguyen16 <sonnv2212@gmail.com>
2024-10-04 21:43:20 +0700

    update

--- backend/database/migrations/0001_01_01_000000_create_users_table.php
+++ backend/database/migrations/0001_01_01_000000_create_users_table.php
@@ -30,7 +30,8 @@ return new class extends Migration
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
+            $table->id();
+            $table->string('email');
             $table->string('token');
             $table->timestamp('created_at')->nullable();
         });
