1225a70bfbe9dd962d006f9e43504b65d2ef458f
sonnguyen16 <sonnv2212@gmail.com>
2024-10-04 21:41:25 +0700

    update

--- backend/database/migrations/2024_09_05_030628_create_orders_table.php
+++ backend/database/migrations/2024_09_05_030628_create_orders_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->string('code')->unique();
+            $table->string('code');
             $table->foreignId('user_id')->unsigned();
             $table->string('name');
             $table->string('phone');
