0c301543c5c1828e2c085afda05c1c018520ad6f
sonnguyen16 <sonnv2212@gmail.com>
2024-10-04 21:36:31 +0700

    update

--- backend/database/migrations/0001_01_01_000000_create_users_table.php
+++ backend/database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,10 +14,10 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->nullable();
+            $table->string('email')->unique()->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('phone');
+            $table->string('phone')->unique();
             $table->integer('province')->nullable();
             $table->integer('district')->nullable();
             $table->integer('ward')->nullable();
